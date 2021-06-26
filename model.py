1db0582cb828ce9f6d75d00954bb5789b6166e82
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-06-26 09:54:31 +0530

    updated model.py and train.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
+import keras
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_addons as tfa
 
 
 class Patches(layers.Layer):
@@ -32,47 +32,58 @@ class PatchEncoder(layers.Layer):
             input_dim=num_patches, output_dim=projection_dim
         )
 
-    def call(self, patch):
+    def call(self, patch, **kwargs):
         positions = tf.range(start=0, limit=self.num_patches, delta=1)
         encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
 
 
-class VisionTransformer(tf.keras.Model):
-    def __init__(self, input_shape, image_size, patch_size, num_patches,
-                 projection_dim, num_heads, transformer_units,
-                 transformer_layers, mlp_head_units, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    super(VisionTransformer, self).__init__()
-
-    def mlp(x, hidden_units, dropout_rate):
-        for units in hidden_units:
-            x = layers.Dense(units, activation=tf.nn.gelu)(x)
-            x = layers.Dropout(dropout_rate)(x)
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
         return x
 
-    inputs = layers.Input(shape=self.input_shape)
-    patches = Patches(self.patch_size)(inputs)
-    encoded_patches = PatchEncoder(self.num_patches, self.projection_dim) \
-        (patches)
-    for _ in range(self.transformer_layers):
-        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-        attention_output = layers.MultiHeadAttention(
-            num_heads=self.num_heads, key_dim=self.projection_dim, dropout=0.1
-        )(x1, x1)
-        x2 = layers.Add()([attention_output, encoded_patches])
-        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-        x3 = mlp(x3, hidden_units=self.transformer_units, dropout_rate=0.1)
-        encoded_patches = layers.Add()([x3, x2])
 
-    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-    representation = layers.Flatten()(representation)
-    representation = layers.Dropout(0.5)(representation)
-    # Add MLP.
-    features = mlp(representation, hidden_units=mlp_head_units,
-                   dropout_rate=0.5)
-    # Classify outputs.
-    logits = layers.Dense(num_classes)(features)
-    # Create the Keras model.
-    model = keras.Model(inputs=inputs, outputs=logits)
+class VisionTransformer(tf.keras.Model):
+    def __init__(self, inputshape, patch_size, num_patches, projection_dim,
+                 transformer_layers, num_heads, transformer_units,
+                 mlp_head_units, num_classes):
+        super(VisionTransformer, self).__init__()
+        self.inputshape = inputshape
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.projection_dim = projection_dim
+        self.transformer_layers = transformer_layers
+        self.num_heads = num_heads
+        self.transformer_units = transformer_units
+        self.mlp_head_units = mlp_head_units
+        self.num_classes = num_classes
+
+    def call(self, input, training):
+        inputs = layers.Input(shape=self.inputshape)
+        patches = Patches(self.patch_size)(inputs)
+        encoded_patches = PatchEncoder(self.num_patches, self.projection_dim)
+        (patches)
+
+        for _ in range(self.transformer_layers):
+            x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            attention_output = layers.MultiHeadAttention(
+                num_heads=self.num_heads, key_dim=self.projection_dim,
+                dropout=0.1
+            )(x1, x1)
+            x2 = layers.Add()([attention_output, encoded_patches])
+            x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+            x3 = mlp(x3, hidden_units=self.transformer_units, dropout_rate=0.1)
+            encoded_patches = layers.Add()([x3, x2])
+
+        representation = layers.LayerNormalization(epsilon=1e-6)(
+            encoded_patches)
+        representation = layers.Flatten()(representation)
+        representation = layers.Dropout(0.5)(representation)
+
+        features = mlp(representation, hidden_units=self.mlp_head_units,
+                       dropout_rate=0.5)
+        logits = layers.Dense(self.num_classes)(features)
+        model = keras.Model(inputs=inputs, outputs=logits)
+        return model
--- train.py
+++ train.py
@@ -1,26 +1,53 @@
-import tesorflow as tf
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_addons
+import tensorflow_addons as tfa
 from model import VisionTransformer
 
 num_classes = 10
-input_shape = (32, 32, 3)
+inputshape = (32, 32, 3)
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
 learning_rate = 0.001
 weight_decay = 0.0001
 batch_size = 256
-num_epochs = 10
-image_size = 72  # We'll resize input images to this size
-patch_size = 6  # Size of the patches to be extract from the input images
+num_epochs = 1
+image_size = 72
+patch_size = 6
 num_patches = (image_size // patch_size) ** 2
 projection_dim = 64
 num_heads = 4
 transformer_units = [
     projection_dim * 2,
     projection_dim,
-]  # Size of the transformer layers
+]
 transformer_layers = 8
 mlp_head_units = [2048, 1024]
+
+model = VisionTransformer(
+    inputshape,
+    patch_size,
+    num_patches,
+    projection_dim,
+    transformer_layers,
+    num_heads,
+    transformer_units,
+    mlp_head_units,
+    num_classes
+)
+
+optimizer = tfa.optimizers.AdamW(
+    learning_rate=learning_rate, weight_decay=weight_decay
+)
+
+model.compile(optimizer=optimizer,
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=[
+                  keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                  keras.metrics.SparseTopKCategoricalAccuracy(5,
+                                                              name="top-5-accuracy"),
+              ],
+              )
+model.build((32, 32, 3))
+model.summary()
