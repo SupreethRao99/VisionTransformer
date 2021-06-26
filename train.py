735e06a97ad053cb7f085e41c6732eb57dbd8ffd
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-06-26 13:38:34 +0530

    Added distributed training feature

--- train.py
+++ train.py
@@ -1,17 +1,16 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 import tensorflow_addons as tfa
+from tensorflow import keras
+import tensorflow_datasets as tfds
 from model import VisionTransformer
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 num_classes = 10
 inputshape = (32, 32, 3)
-
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-
 learning_rate = 0.001
 weight_decay = 0.0001
-batch_size = 256
+batch_size = 32
 num_epochs = 1
 image_size = 72
 patch_size = 6
@@ -25,29 +24,54 @@ transformer_units = [
 transformer_layers = 8
 mlp_head_units = [2048, 1024]
 
-model = VisionTransformer(
-    inputshape,
-    patch_size,
-    num_patches,
-    projection_dim,
-    transformer_layers,
-    num_heads,
-    transformer_units,
-    mlp_head_units,
-    num_classes
+ds = tfds.load("cifar10", as_supervised=True)
+ds_train = (
+    ds["train"]
+        .cache()
+        .shuffle(5 * batch_size)
+        .batch(batch_size)
+        .prefetch(AUTOTUNE)
+)
+ds_test = (
+    ds["test"]
+        .cache()
+        .batch(batch_size)
+        .prefetch(AUTOTUNE)
 )
 
-optimizer = tfa.optimizers.AdamW(
-    learning_rate=learning_rate, weight_decay=weight_decay
-)
+strategy = tf.distribute.MirroredStrategy()
 
-model.compile(optimizer=optimizer,
-              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=[
-                  keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
-                  keras.metrics.SparseTopKCategoricalAccuracy(5,
-                                                              name="top-5-accuracy"),
-              ],
-              )
-model.build((32, 32, 3))
-model.summary()
+with strategy.scope():
+    model = VisionTransformer(
+        inputshape,
+        patch_size,
+        num_patches,
+        projection_dim,
+        transformer_layers,
+        num_heads,
+        transformer_units,
+        mlp_head_units,
+        num_classes
+    )
+
+    optimizer = tfa.optimizers.AdamW(
+        learning_rate=learning_rate, weight_decay=weight_decay
+    )
+
+    model.compile(optimizer=optimizer,
+                  loss=keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=[
+                      keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                      keras.metrics.SparseTopKCategoricalAccuracy(5,
+                                                                  name="top-5"
+                                                                       "-accuracy"),
+                  ],
+                  )
+
+    history = model.fit(
+        ds_train,
+        validation_data=ds_train,
+        batch_size=batch_size,
+        epochs=num_epochs,
+    )
